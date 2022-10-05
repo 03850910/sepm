d4f006516ddb4ed5343fd4a00bed7211acd3774b
Gary <48701507+s3720380@users.noreply.github.com>
2022-10-06 01:40:15 +1100

    Work on PBI 1, 3 & 4
    Styling + Group Creation (Obj to Array) + Printing +Downloading

--- teams.js
+++ teams.js
@@ -1,71 +1,38 @@
-$(document).ready(function () {
+$(document).ready(function() {
     const studentsdata = [];
     const header = [];
     const workshops = {};
     const workshopteams = {};
     const minStudents = 5;
     const maxStudents = 7;
+    let groups = [];
+    let prunedgroups = [];
     const projectoptions = ["Web-app", "iOS app", "Android app", "desktop application"];
     const workshopnames = ["WRK01/01", "WRK01/02", "WRK01/03", "WRK01/04", "WRK01/05", "WRK01/06", "WRK01/07", "WRK01/08", "WRK01/09", "WRK01/10", "WRK01/11", "WRK01/12"];
 
     //below function uploads a file and presents it in a tabular format 
-    const uploadconfirm = document.getElementById('uploadconfirm').addEventListener('click', () => {
-        Papa.parse(document.getElementById('uploadfile').files[0],
-            {
-                download: true,
-                skipEmptyLines: true,
+    uploadconfirm = document.getElementById('uploadconfirm').addEventListener('click', () => {
+        document.getElementById("uploadconfirm").hidden = true
+        console.log('Student preferences uploaded successfully');
+        Papa.parse(document.getElementById('uploadfile').files[0], {
+            download: true,
+            skipEmptyLines: true,
 
-                complete: function (results) {
-                    for (i = 0; i < results.data.length; i++) {
-                        if (i === 0) {
-                            header.push(results.data[i])
-                            continue
-                        } else {
-                            studentsdata.push(results.data[i])
-                        }
+            complete: function(results) {
+                for (i = 0; i < results.data.length; i++) {
+                    if (i === 0) {
+                        header.push(results.data[i])
+                        continue
+                    } else {
+                        studentsdata.push(results.data[i])
                     }
-                    //console.log(studentsdata)
-                    csvtable = `
-                    <table class="table table-bordered text-white">
-                        <thead class="thead-dark">
-                            <tr id="header">
-                            </tr>
-                        </thead>
-                        <tbody id="table">   
-                        </tbody>
-                    </table>         
-                     `;
-                    document.querySelector("#table-container").innerHTML = csvtable;
-
-                    let headerrow = "";
-                    for (i = 0; i < header[0].length; i++) {
-                        headerrow += `
-                                            <th scope="col">${header[0][i]}</th>          
-                                      `;
-                    }
-                    document.querySelector("#header").innerHTML = headerrow;
-
-                    let j = 0;
-                    let row = "";
-                    let tablecontent = "";
-                    for (i = 0; i < studentsdata.length; i++) {
-                        j = 0;
-                        while (j < studentsdata[i].length) {
-                            row += `
-                                     <td>${studentsdata[i][j]}</td>                                                   
-                               `;
-
-                            j++;
-                        }
-                        tablecontent += `</th>` + row + "</tr>";
-                        row = "";
-                    }
-                    document.getElementById('table').innerHTML = tablecontent;
-                    addStudentsToWorkShops();
-
-
                 }
-            })
+                //console.log(studentsdata);
+                addStudentsToWorkShops();
+                createGroups();
+                printWorkshopTeams();
+            }
+        })
     })
 
 
@@ -83,113 +50,73 @@ $(document).ready(function () {
         for (let student of studentsdata) {
             workshops[student[workshopcolumn]].push(student);
         }
-        sortProjectPrefernces();
+        sortProjectPreferences();
         //console.log(workshops);
     }
 
-    function sortProjectPrefernces() {
+    function sortProjectPreferences() {
         let optioncolumn = 9;
         let projectteams = {};
         assignWorkshopName(workshopteams);
-        for (let option of  projectoptions) {
+        for (let option of projectoptions) {
             projectteams[option] = [];
         }
         for (let workshop in workshops) {
             for (let student of workshops[workshop]) {
                 let projectoption = student[optioncolumn].replace(";", "");
                 projectteams[projectoption].push(student);
-                if(projectteams[projectoption].length === minStudents){
-                 //   workshopteams[workshop] = [];
+                if (projectteams[projectoption].length === minStudents) {
                     workshopteams[workshop].push(projectteams[projectoption]);
-                    projectteams[projectoption]=[];
+                    projectteams[projectoption] = [];
                 }
             }
-            
+
         }
-        console.log(workshopteams);
+        //console.log(workshopteams);
     }
 
-    function makeWorkshopTeams() {
-
-    }
-
-    function makeTeams() {
-        // Initialise constants for Sprint 1, not customisable yet.
-        const maxTeams = 5;
- 
-        // Set all workshops into an array
-        const wrkShops = ["WRK01/01", "WRK01/02", "WRK01/03", "WRK01/04", "WRK01/05", "WRK01/06", "WRK01/07", "WRK01/08", "WRK01/09", "WRK01/10", "WRK01/11", "WRK01/12", "Not enrolled in workshop class yet"];
-        const teams = [];
-        // Iterate through all workshop classes to sort students by workshop
-        for (var i = 0; i < wrkShops.length; i++) {
-            // Create empty array for students to be sorted into
-            var students = [];
-            // Iterate through 2D array of students from the csv
-            for (var k = 0; k < studentsdata.length; k++) {
-                // Add students with matching workshop class to the array
-                if (studentsdata[k][7] == wrkShops[i]) {
-                    students.push(studentsdata[k]);
-                }
-            }
-            // Work on sorted students here
-            for (var l = 0; l < 5; l++) {
-                var currentTeam = [];
-                if (students[l] == undefined) {
-                    break;
-                }
-                else if (l == 0) {
-                    for (var z = 0; z < 7; z++) {
-                        students[z].unshift("1");
-                        currentTeam.push(students[z]);
-                    }
-                    teams.push(currentTeam);
-                }
-                else if (l == 1) {
-                    for (var z = 7; z < 14; z++) {
-                        students[z].unshift("2");
-                        currentTeam.push(students[z]);
-                    }
-                    teams.push(currentTeam);
-                }
-                else if (l == 2) {
-                    for (var z = 14; z < 21; z++) {
-                        students[z].unshift("3");
-                        currentTeam.push(students[z]);
-                    }
-                    teams.push(currentTeam);
-                }
-                else if (l == 3) {
-                    for (var z = 21; z < 28; z++) {
-                        students[z].unshift("4");
-                        currentTeam.push(students[z]);
-                    }
-                    teams.push(currentTeam);
-                }
-                else if (l == 4) {
-                    for (var z = 28; z < 35; z++) {
-                        students[z].unshift("5");
-                        currentTeam.push(students[z]);
-                    }
-                    teams.push(currentTeam);
-                }
-            }
-        }
-        console.log(teams.toString());
-
-    }
-
-    function toCSV(inputArray, separator = ",") {
-        let rowsAsString = inputArray.map(row => {
-            return row.join(separator);
+    function createGroups() {
+        //console.log(JSON.stringify(workshopteams, null, 3))
+        groups = $.map(workshopteams, function(value) {
+            return [value]
         })
-        return csvFormat = rowsAsString.join("\n");
+        console.log(groups);
+
+        for (i = 0; i < groups.length; i++) {
+            prunedgroups = prunedgroups.concat(groups[i]);
+        }
+        console.log(prunedgroups);
+    }
+
+    function printWorkshopTeams() {
+        csvtable = `
+                    <table class="table table-bordered text-white">
+                        <tbody id="table">   
+                        </tbody>
+                    </table>         
+                     `;
+        document.querySelector("#table-container").innerHTML = csvtable;
+
+        let j = 0;
+        let k = 0;
+        let row = "";
+        let tablecontent = "";
+
+        for (i = 0; i < prunedgroups.length; i++) {
+            j = 0;
+            k++;
+            while (j < prunedgroups[i].length) {
+                row += `
+                         <td>${prunedgroups[i][j]}</td>                                                   
+                   `;
+
+                j++;
+            }
+            let string = "Group: " + k
+            tablecontent += `</th>` + string + row + "</tr>";
+            row = "";
+        }
+        document.getElementById('table').innerHTML = tablecontent;
     }
 
 });
-
-
-//begin code here
-//the data extracted from teh csv file is put into the 'studentdata' 2d array 
-
-
-
