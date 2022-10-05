b8bb236ac4494867ecca0c80efde4326690fd283
Fisseha Abate 3850910 <s3850910@student.rmit.edu.au>
2022-10-05 18:17:28 +1100

    preferences

--- teams.js
+++ teams.js
@@ -2,6 +2,12 @@ $(document).ready(function () {
     const studentsdata = [];
     const header = [];
     const workshops = {};
+    const workshopteams = {};
+    const minStudents = 5;
+    const maxStudents = 7;
+    const projectoptions = ["Web-app", "iOS app", "Android app", "desktop application"];
+    const workshopnames = ["WRK01/01", "WRK01/02", "WRK01/03", "WRK01/04", "WRK01/05", "WRK01/06", "WRK01/07", "WRK01/08", "WRK01/09", "WRK01/10", "WRK01/11", "WRK01/12"];
+
     //below function uploads a file and presents it in a tabular format 
     const uploadconfirm = document.getElementById('uploadconfirm').addEventListener('click', () => {
         Papa.parse(document.getElementById('uploadfile').files[0],
@@ -65,37 +71,52 @@ $(document).ready(function () {
 
     //begin code here
     //the data extracted from the csv file is put into the 'studentdata' 2d array 
-    function makeWorkshops() {
-        let workshopnames = ["WRK01/01", "WRK01/02", "WRK01/03", "WRK01/04", "WRK01/05", "WRK01/06", "WRK01/07", "WRK01/08", "WRK01/09", "WRK01/10", "WRK01/11", "WRK01/12"];
-        
+    function assignWorkshopName(someobject) {
         for (let workshopname of workshopnames) {
-            workshops[workshopname] = [];
+            someobject[workshopname] = [];
         }
     }
 
     function addStudentsToWorkShops() {
-        makeWorkshops();
+        assignWorkshopName(workshops);
         let workshopcolumn = 7;
-
-        for (const student of studentsdata) {
+        for (let student of studentsdata) {
             workshops[student[workshopcolumn]].push(student);
         }
+        sortProjectPrefernces();
         //console.log(workshops);
     }
 
-    function sortProjectPrefernces(){
-        
+    function sortProjectPrefernces() {
+        let optioncolumn = 9;
+        let projectteams = {};
+        assignWorkshopName(workshopteams);
+        for (let option of  projectoptions) {
+            projectteams[option] = [];
+        }
+        for (let workshop in workshops) {
+            for (let student of workshops[workshop]) {
+                let projectoption = student[optioncolumn].replace(";", "");
+                projectteams[projectoption].push(student);
+                if(projectteams[projectoption].length === minStudents){
+                 //   workshopteams[workshop] = [];
+                    workshopteams[workshop].push(projectteams[projectoption]);
+                    projectteams[projectoption]=[];
+                }
+            }
+            
+        }
+        console.log(workshopteams);
     }
 
-    function makeWorkshopTeams(){
-        
+    function makeWorkshopTeams() {
+
     }
 
     function makeTeams() {
         // Initialise constants for Sprint 1, not customisable yet.
         const maxTeams = 5;
-        var minStudents = 5;
-        var maxStudents = 7;
+ 
         // Set all workshops into an array
         const wrkShops = ["WRK01/01", "WRK01/02", "WRK01/03", "WRK01/04", "WRK01/05", "WRK01/06", "WRK01/07", "WRK01/08", "WRK01/09", "WRK01/10", "WRK01/11", "WRK01/12", "Not enrolled in workshop class yet"];
         const teams = [];
